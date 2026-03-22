1061ef5f4392c9b917234bed7e765a12aaf3f9a4
Max Gramser <maxgramser@gmail.com>
2026-03-22 15:32:06 +0100

    bugfix

--- custom_components/timey/config_flow.py
+++ custom_components/timey/config_flow.py
@@ -8,7 +8,7 @@ from typing import Any
 import aiohttp
 import voluptuous as vol
 
-from homeassistant.components.zeroconf import ZeroconfServiceInfo
+from homeassistant.helpers.service_info.zeroconf import ZeroconfServiceInfo
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 
 from .const import API_STATE, CONF_HOST, DOMAIN
